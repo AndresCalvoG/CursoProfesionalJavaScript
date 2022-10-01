5f00f3208bc582f73e30f27bb1ae4adca5a96e67
Andres Calvo <andrescalvo9407@gmail.com>
2022-10-01 15:21:54 -0500

    change of repository

---
+++ ejercicios/typeScript/mediaPlayerTS/index.js
@@ -0,0 +1,26 @@
+import MediaPlayer from "./mediaPlayer.js";
+import AutoPlay from "./plugins/Autoplay.js";
+import AutoPause from "./plugins/AutoPause.ts";
+
+const video = document.querySelector("video");
+const button = document.querySelector("button");
+const mute = document.querySelector("#sound");
+
+const player = new MediaPlayer({
+  element: video,
+  plugins: [new AutoPlay(), new AutoPause()],
+});
+
+button.onclick = () => {
+  player.togglePlay();
+};
+
+mute.onclick = () => {
+  player.toggleMute();
+};
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/serviceWorker.js").catch((error) => {
+    console.log(error.message);
+  });
+}
