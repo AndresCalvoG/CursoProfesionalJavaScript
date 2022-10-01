2d37cdd73b1d22ec1959cdf931036f86d275ba35
Andres Calvo <andrescalvo9407@gmail.com>
2022-10-01 16:51:10 -0500

    end class 29 convert project to typescript

---
+++ ejercicios/typeScript/mediaPlayerTS/index.ts
@@ -0,0 +1,24 @@
+import MediaPlayer from "./mediaPlayer";
+import AutoPlay from "./plugins/Autoplay";
+import AutoPause from "./plugins/AutoPause";
+
+const video = document.querySelector("video");
+
+const player = new MediaPlayer({
+  element: video,
+  plugins: [new AutoPlay(), new AutoPause()],
+});
+
+const playButton: HTMLElement = document.querySelector("#playButton");
+playButton.onclick = () => player.togglePlay();
+
+const muteButton: HTMLElement = document.querySelector("#muteButton");
+muteButton.onclick = () => player.toggleMute();
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("../../../serviceWorker.js")
+    .catch((error) => {
+      console.log(error.message);
+    });
+}
--- ejercicios/typeScript/mediaPlayerTS/mediaPlayer.ts
+++ ejercicios/typeScript/mediaPlayerTS/mediaPlayer.ts
@@ -1,24 +1,15 @@
 class MediaPlayer {
+  media: HTMLMediaElement;
+  plugins: Array<any>;
+
   constructor(config) {
     this.media = config.element;
     this.plugins = config.plugins || [];
-    this._initPlugin();
+    this.initPlugin();
   }
-  _initPlugin() {
-    const player = {
-      play: () => this.play(),
-      pause: () => this.pause(),
-      media: this.media,
-      get muted() {
-        return this.media.muted;
-      },
-      set muted(value) {
-        this.media.muted = value;
-      },
-    };
-
+  private initPlugin() {
     this.plugins.forEach((plugin) => {
-      plugin.run(player);
+      plugin.run(this);
     });
   }
   play() {
